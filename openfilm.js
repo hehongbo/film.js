fc3fd25dcb93e4eba0b6db67f37fd55787bf6353
HeHongbo <hehongbo@mail.com>
2019-12-02 19:32:33 +0800

    Construct basic data structure and canvas elements

--- openfilm.js
+++ openfilm.js
@@ -1,4 +1,38 @@
 class OpenFilm {
+    frameImgArr = [];
+    targetElementName;
+
     constructor(targetElementName, layers, options) {
+        this.targetElementName = targetElementName;
+        layers.forEach(function (layer, id) {
+            let imgArr = [];
+            layer.frames.forEach(function (url) {
+                let imgElement = document.createElement("img");
+                imgElement.src = url;
+                imgArr = imgArr.concat([imgElement]);
+            });
+            this.frameImgArr = this.frameImgArr.concat([imgArr]);
+            let canvasElement = document.createElement("canvas");
+            canvasElement.height = options.height;
+            canvasElement.width = options.width;
+            canvasElement.style.width = "100%";
+            canvasElement.style.height = "100%";
+            canvasElement.style.objectFit = "cover";
+            if (id !== 0) {
+                canvasElement.style.position = "absolute";
+                canvasElement.style.top = "0";
+                canvasElement.style.left = "0";
+                canvasElement.zIndex = id;
+            }
+            document.getElementById(targetElementName).appendChild(canvasElement);
+        }.bind(this));
+
+        document.getElementById(targetElementName).style.position = "relative";
+        document.getElementById(targetElementName).style.objectFit = "cover";
+
+        this.requireFrame(0);
+    }
+
+    requireFrame(frameN) {
     }
 }
