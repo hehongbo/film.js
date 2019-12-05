143b03354108f23b6dce9c3a0b5c15c7fda8fc7b
HeHongbo <hehongbo@mail.com>
2019-12-05 10:44:25 +0800

    Fix the bug where unprepared frames can not be handled before calling drawImage()

--- openfilm.js
+++ openfilm.js
@@ -3,6 +3,9 @@ class OpenFilm {
         // Receive `targetElementName` (the name of `div` element that holds canvas) during construct for future use.
         this.targetElementName = targetElementName;
         layers.forEach(function (layer, id) {
+            // Create a counter for frames to load (on `id == 0`) or add up the counter (on `id > 0`).
+            this.unpreparedFrameCount = (typeof this.unpreparedFrameCount !== 'undefined') ?
+                this.unpreparedFrameCount + layer.frames.length : layer.frames.length;
             // Copy everything from the `layers` parameter, except frames[] is made up of img element(s) instead of their URL.
             let layerSlice = {
                 patch: layer.patch,
@@ -12,10 +15,18 @@ class OpenFilm {
                 frames: []
             };
             layer.frames.forEach(function (url) {
+                // Create an img element.
                 let imgElement = document.createElement("img");
+                // Add a one-time event listener before putting the URL into imgElement's attribute. This will result in
+                // the image being load. When loading is complete, the registered callback function will decrease the
+                // counter by one. So `unpreparedFrameCount === 0` indicates that assets of this instance are fully
+                // loaded.
+                imgElement.addEventListener("load", function () {
+                    this.unpreparedFrameCount = this.unpreparedFrameCount - 1;
+                }.bind(this), {once: true});
                 imgElement.src = url;
                 layerSlice.frames = layerSlice.frames.concat([imgElement]);
-            });
+            }.bind(this));
             // Append slice into this.layers[], and create one with first slice if not existed.
             this.layers = (typeof this.layers !== 'undefined') ? this.layers.concat([layerSlice]) : [layerSlice];
 
@@ -49,37 +60,40 @@ class OpenFilm {
     requireFrame(frameN) {
         // Failsafe for handling non-integer inputs from scrolling libraries.
         let round = !Number.isInteger(frameN);
-        // Loop through all layers.
-        this.layers.forEach(function (layer, id) {
-            // We use the `currentFrame` field to indicate the current frame's number, compare with required ones during refresh,
-            // and store new value when complete. When calling this method for the first time, it will be 'undefined'.
-            if (typeof this.currentFrame === 'undefined') {
-                document.getElementById(this.targetElementName).children[id].getContext("2d").drawImage(
-                    !round ? layer.frames[frameN] : layer.frames[Math.round(frameN)],
-                    layer.patch ? layer.position.left : 0,
-                    layer.patch ? layer.position.top : 0);
-            } else if (frameN <= layer.startFrame) { // When the required frame is ahead of this layer's time range.
-                if (this.currentFrame > layer.startFrame) { // And was in between this layer's time range before.
+        // Make sure there's no unprepared frames or otherwise `drawImage()` will fail.
+        if (this.unpreparedFrameCount === 0) {
+            // Loop through all layers.
+            this.layers.forEach(function (layer, id) {
+                // We use the `currentFrame` field to indicate the current frame's number, compare with required ones during refresh,
+                // and store new value when complete. When calling this method for the first time, it will be 'undefined'.
+                if (typeof this.currentFrame === 'undefined') {
                     document.getElementById(this.targetElementName).children[id].getContext("2d").drawImage(
-                        layer.frames[layer.startFrame], // Point to the first frame and stay here until going into this range.
+                        !round ? layer.frames[frameN] : layer.frames[Math.round(frameN)],
                         layer.patch ? layer.position.left : 0,
                         layer.patch ? layer.position.top : 0);
-                }
-            } else if (frameN > layer.startFrame && frameN < layer.endFrame) { // When required frame is in this range.
-                document.getElementById(this.targetElementName).children[id].getContext("2d").drawImage(
-                    !round ? layer.frames[frameN] : layer.frames[Math.round(frameN)],
-                    layer.patch ? layer.position.left : 0,
-                    layer.patch ? layer.position.top : 0);
-            } else { // When required frame is behind this layer's time range. (Last possible condition)
-                if (this.currentFrame < layer.endFrame) { // And was in between this layer's time range before.
+                } else if (frameN <= layer.startFrame) { // When the required frame is ahead of this layer's time range.
+                    if (this.currentFrame > layer.startFrame) { // And was in between this layer's time range before.
+                        document.getElementById(this.targetElementName).children[id].getContext("2d").drawImage(
+                            layer.frames[layer.startFrame], // Point to the first frame and stay here until going into this range.
+                            layer.patch ? layer.position.left : 0,
+                            layer.patch ? layer.position.top : 0);
+                    }
+                } else if (frameN > layer.startFrame && frameN < layer.endFrame) { // When required frame is in this range.
                     document.getElementById(this.targetElementName).children[id].getContext("2d").drawImage(
-                        layer.frames[layer.endFrame], // Point to the last frame and stay here until going into this range.
+                        !round ? layer.frames[frameN] : layer.frames[Math.round(frameN)],
                         layer.patch ? layer.position.left : 0,
                         layer.patch ? layer.position.top : 0);
+                } else { // When required frame is behind this layer's time range. (Last possible condition)
+                    if (this.currentFrame < layer.endFrame) { // And was in between this layer's time range before.
+                        document.getElementById(this.targetElementName).children[id].getContext("2d").drawImage(
+                            layer.frames[layer.endFrame], // Point to the last frame and stay here until going into this range.
+                            layer.patch ? layer.position.left : 0,
+                            layer.patch ? layer.position.top : 0);
+                    }
                 }
-            }
-        }.bind(this));
-        // Store new frame number.
-        this.currentFrame = !round ? frameN : Math.round(frameN);
+            }.bind(this));
+            // Store new frame number.
+            this.currentFrame = !round ? frameN : Math.round(frameN);
+        }
     }
 }
